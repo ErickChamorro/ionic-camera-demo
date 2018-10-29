691cf4102a571ff0d940e91964e157154dc79990
ErickChamorro <eschamorro97@misena.edu.co>
2018-10-29 11:59:33 -0500

    preparando para exportar apk

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, Platform, MenuController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { CalendarPage } from '../calendar/calendar';
+import { MapaPage } from '../mapa/mapa';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,58 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  image: string = null;
 
+  constructor(
+    public navCtrl: NavController,
+    private camera: Camera,
+    public alertCtrl: AlertController,
+    public platform: Platform,
+    public menuCtrl: MenuController) {
+    this.platform = platform;
   }
 
+  enviar(){
+    const alerta = this.alertCtrl.create({
+      title: 'Exito',
+      message: 'Su imagen ha sido enviada',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: data =>{
+            this.platform.exitApp();
+          }
+        }
+      ]
+    });
+    alerta.present();
+  }
+
+  getPicture(){
+    let options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL,
+      targetWidth: 100,
+      targetHeight: 100,
+      quality: 100
+    }
+    this.camera.getPicture( options )
+    .then(imageData => {
+      this.image = `data:image/jpeg;base64,${imageData}`;
+    })
+    .catch(error => {
+      console.log( error );
+    });
+  }
+
+  llevame_a_calendar(){
+    this.navCtrl.push(CalendarPage);
+  }
+
+  llevame_a_mapa(){
+    this.navCtrl.push(MapaPage);
+  }
+    
+  openMenu(){
+    this.menuCtrl.toggle();
+  }
 }
